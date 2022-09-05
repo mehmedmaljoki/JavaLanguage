36840dd5d73fe5b1c35d87ba0350c05f485dcf38
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2022-09-05 21:40:18 +0200

    adding lenera search

--- src/ArrayList_09/LinearSearch.java
+++ src/ArrayList_09/LinearSearch.java
@@ -1,4 +1,20 @@
 package ArrayList_09;
 
 public class LinearSearch {
+	public static void main(String[] args) {
+		int[] arr = {2, 4, 56, 3, 6, 3, 6, 3, 6, 2, 54, 252, 43, 2, 5};
+		System.out.println("position: " + linearSearch(arr, 4));
+	}
+
+	public static int linearSearch(int[] arr, int target) {
+		if (arr.length == 0) {
+			return -1;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == target) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
