6137c1ddecbcdf20579df7d5439e7d7b65fac2ae
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2022-09-06 21:29:44 +0200

    adding ceiling floor number in an array

--- src/ArrayList_09/BinarySearch.java
+++ src/ArrayList_09/BinarySearch.java
@@ -3,7 +3,10 @@ package ArrayList_09;
 public class BinarySearch {
 	public static void main(String[] args) {
 		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		System.out.println(binarySearch(arr, 5));
+		System.out.println(binarySearch(arr, 11));
+
+		int[] arr2 = {2, 3, 5, 9, 14, 16, 18};
+		System.out.println(ceiling(arr2, 4));
 
 	}
 
@@ -62,5 +65,47 @@ public class BinarySearch {
 
 		}
 		return -1;
+	}
+
+	// ceiling number = smallest element in array >= target
+	public static int ceiling(int[] arr, int target) {
+		if (arr.length == 0) return -1;
+
+		int start = 0;
+		int end = arr.length - 1;
+
+		while (start <= end) {
+			int middle = start + (end - start) / 2; // same as (start +end) / 2
+
+			if (target < arr[middle]) {
+				end = middle - 1;
+			} else if (target > arr[middle]) {
+				start = middle + 1;
+			} else {
+				return arr[middle];
+			}
+		}
+		return arr[start];
+	}
+
+	// floor = greatest number <= target
+	public static int floor(int[] arr, int target) {
+		if (arr.length == 0) return -1;
+
+		int start = 0;
+		int end = arr.length - 1;
+
+		while (start <= end) {
+			int middle = start + (end - start) / 2; // same as (start +end) / 2
+
+			if (target < arr[middle]) {
+				end = middle - 1;
+			} else if (target > arr[middle]) {
+				start = middle + 1;
+			} else {
+				return arr[middle];
+			}
+		}
+		return arr[end];
 	}
 }
